80c127b4720160eee4506ed84c8f7f4af7298929
Julio Brito <jucebridu@gmail.com>
2018-01-19 17:31:19 -0500

    se implemta tempaltes, views y urls para notificaciones

--- notificaciones/urls.py
+++ notificaciones/urls.py
@@ -2,11 +2,27 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import url
+from .views import (
+ListNotificacionRequiereVisita,
+SearchNotificacionRequiereVisita,
+ListNotificacionFallaInstalacion,
+SearchNotificacionFallaInstalacion,
+ListNotificacionFallaIntegracion,
+SearchNotificacionFallaIntegracion,
+)
 from . import views
 
 app_name = 'notificaciones'
 urlpatterns = [
+    url(r'^list/notificacion/requiere_visita/$', ListNotificacionRequiereVisita.as_view(), name='list_notificacion_requiere_visita'),
+    url(r'^search/notificacion/requiere_visita/$', SearchNotificacionRequiereVisita.as_view(), name='search_notificacion_requiere_visita'),
     url(r'^export/notificaciones/requiere_visita/$', views.export_notificaciones_requiere_visita, name='export_notificaciones_requiere_visita'),
+
+    url(r'^list/notificacion/falla_instalacion/$', ListNotificacionFallaInstalacion.as_view(), name='list_notificacion_falla_instalacion'),
+    url(r'^search/notificacion/falla_instalacion/$', SearchNotificacionFallaInstalacion.as_view(), name='search_notificacion_falla_instalacion'),
     url(r'^export/notificaciones/falla_instalacion/$', views.export_notificaciones_falla_instalacion, name='export_notificaciones_falla_instalacion'),
+
+    url(r'^list/notificacion/falla_integracion/$', ListNotificacionFallaIntegracion.as_view(), name='list_notificacion_falla_integracion'),
+    url(r'^search/notificacion/falla_integracion/$', SearchNotificacionFallaIntegracion.as_view(), name='search_notificacion_falla_integracion'),
     url(r'^export/notificaciones/falla_integracion/$', views.export_notificaciones_falla_integracion, name='export_notificaciones_falla_integracion'),
 ]
--- notificaciones/views.py
+++ notificaciones/views.py
@@ -11,6 +11,58 @@ NotificacionFallaInstalacionResource,
 NotificacionFallaIntegracionResource,
 )
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+
+import operator
+from django.db.models import Q
+from functools import reduce
+
+class ListNotificacionRequiereVisita(LoginRequiredMixin, ListView):
+    login_url = 'users:login_user'
+    model = NotificacionRequiereVisita
+    template_name = 'notificacion_requiere_visita/list_notificacion_requiere_visita.html'
+    paginate_by = 100
+
+class SearchNotificacionRequiereVisita(ListNotificacionRequiereVisita):
+
+    def get_queryset(self):
+        queryset = super(SearchNotificacionRequiereVisita, self).get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            query_list = query.split()
+            queryset = queryset.filter(
+                reduce(operator.and_,
+                          (Q(id__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(asignacion_ni__id__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(actividad__id__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(wp__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(service_supplier__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(estacion__nombre__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(banda__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(proyecto__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(escenario__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(ni_ingeniero__nombre__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(ni_ingeniero__apellido__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(ni_ingeniero__nombre_completo__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(creado__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(actualizado__icontains=q) for q in query_list))
+            )
+        return queryset
+
 def export_notificaciones_requiere_visita(request):
     notificaciones_requiere_visita = NotificacionRequiereVisitaResource()
     dataset = notificaciones_requiere_visita.export()
@@ -18,12 +70,104 @@ def export_notificaciones_requiere_visita(request):
     response['Content-Disposition'] = 'attachment; filename="NotificacionRequiereVisita.xlsx"'
     return response
 
+class ListNotificacionFallaInstalacion(LoginRequiredMixin, ListView):
+    login_url = 'users:login_user'
+    model = NotificacionFallaInstalacion
+    template_name = 'notificacion_falla_instalacion/list_notificacion_falla_instalacion.html'
+    paginate_by = 100
+
+class SearchNotificacionFallaInstalacion(ListNotificacionFallaInstalacion):
+
+    def get_queryset(self):
+        queryset = super(SearchNotificacionFallaInstalacion, self).get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            query_list = query.split()
+            queryset = queryset.filter(
+                reduce(operator.and_,
+                          (Q(id__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(asignacion_ni__id__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(actividad__id__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(wp__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(service_supplier__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(estacion__nombre__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(banda__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(proyecto__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(escenario__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(ni_ingeniero__nombre__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(ni_ingeniero__apellido__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(ni_ingeniero__nombre_completo__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(solver__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(creado__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(actualizado__icontains=q) for q in query_list))
+            )
+        return queryset
+
 def export_notificaciones_falla_instalacion(request):
     notificaciones_falla_instalacion = NotificacionFallaInstalacionResource()
     dataset = notificaciones_falla_instalacion.export()
     response = HttpResponse(dataset.xlsx, content_type='application/vnd.ms-excel')
     response['Content-Disposition'] = 'attachment; filename="NotificacionFallaInstalacion.xlsx"'
     return response
+
+class ListNotificacionFallaIntegracion(LoginRequiredMixin, ListView):
+    login_url = 'users:login_user'
+    model = NotificacionFallaIntegracion
+    template_name = 'notificacion_falla_integracion/list_notificacion_falla_integracion.html'
+    paginate_by = 100
+
+class SearchNotificacionFallaIntegracion(ListNotificacionFallaIntegracion):
+
+    def get_queryset(self):
+        queryset = super(SearchNotificacionFallaIntegracion, self).get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            query_list = query.split()
+            queryset = queryset.filter(
+                reduce(operator.and_,
+                          (Q(id__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(asignacion_ni__id__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(actividad__id__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(wp__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(service_supplier__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(estacion__nombre__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(banda__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(proyecto__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(escenario__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(ni_ingeniero__nombre__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(ni_ingeniero__apellido__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(ni_ingeniero__nombre_completo__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(creado__icontains=q) for q in query_list)) |
+                reduce(operator.and_,
+                          (Q(actualizado__icontains=q) for q in query_list))
+            )
+        return queryset
 
 def export_notificaciones_falla_integracion(request):
     notificaciones_falla_integracion = NotificacionFallaIntegracionResource()
