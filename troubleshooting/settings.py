11d28fa4df31b71561227184120bbaabf90c50fc
Julio Brito <jucebridu@gmail.com>
2018-01-22 09:53:57 -0500

    se implementa redireccionamiento a https

--- troubleshooting/settings.py
+++ troubleshooting/settings.py
@@ -150,6 +150,16 @@ BUCKET_NAME = os.getenv('BUCKET_NAME')
 EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
+
 try:
     from local_settings import *
 except ImportError:
