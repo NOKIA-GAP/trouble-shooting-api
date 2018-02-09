58ae9f13c2a04d3368903e62ea0a4a297fb7cb92
Julio Brito <jucebridu@gmail.com>
2018-02-09 18:47:44 -0500

    fix error nombre de variable incidentes de contexto

--- incidentes/context_processors.py
+++ incidentes/context_processors.py
@@ -4,36 +4,36 @@ GAP_ADMINISTRADOR = 'GAP Administrador'
 NI_INGENIERO = 'NI Ingeniero'
 NPO_INGENIERO = 'NPO Ingeniero'
 
-def incidentes_npo(request):
-    incidentes_npo = ''
+def notificacion_incidentes_npo(request):
+    notificacion_incidentes_npo = ''
     if request.user.is_authenticated:
         perfil = request.user.perfil
         if perfil.perfil_usuario == GAP_ADMINISTRADOR:
-            incidentes_npo = IncidenteNpo.objects.filter(estado_incidente='Abierto').count()
+            notificacion_incidentes_npo = IncidenteNpo.objects.filter(estado_incidente='Abierto').count()
         if perfil.perfil_usuario == NPO_INGENIERO:
-            incidentes_npo = IncidenteNpo.objects.filter(estado_incidente='Abierto', npo_ingeniero=perfil).count()
+            notificacion_incidentes_npo = IncidenteNpo.objects.filter(estado_incidente='Abierto', npo_ingeniero=perfil).count()
 
         return {
-            'incidentes_npo': incidentes_npo,
+            'notificacion_incidentes_npo': notificacion_incidentes_npo,
         }
     else:
         return {
-            'incidentes_npo': incidentes_npo,
+            'notificacion_incidentes_npo': notificacion_incidentes_npo,
         }
 
-def incidentes_ni(request):
-    incidentes_ni = ''
+def notificacion_incidentes_ni(request):
+    notificacion_incidentes_ni = ''
     if request.user.is_authenticated:
         perfil = request.user.perfil
         if perfil.perfil_usuario == GAP_ADMINISTRADOR:
-            incidentes_ni = IncidenteNi.objects.filter(estado_incidente='Abierto').count()
+            notificacion_incidentes_ni = IncidenteNi.objects.filter(estado_incidente='Abierto').count()
         if perfil.perfil_usuario == NI_INGENIERO:
-            incidentes_ni = IncidenteNi.objects.filter(estado_incidente='Abierto', ni_ingeniero=perfil).count()
+            notificacion_incidentes_ni = IncidenteNi.objects.filter(estado_incidente='Abierto', ni_ingeniero=perfil).count()
 
         return {
-            'incidentes_ni': incidentes_ni,
+            'notificacion_incidentes_ni': notificacion_incidentes_ni,
         }
     else:
         return {
-            'incidentes_ni': incidentes_ni,
+            'notificacion_incidentes_ni': notificacion_incidentes_ni,
         }
