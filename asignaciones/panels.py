7575de6bb3aca1f0504bca44555c59358729cf3a
Julio Brito <jucebridu@gmail.com>
2018-02-15 11:19:13 -0500

    paneles de informacion en progreso

--- asignaciones/panels.py
+++ asignaciones/panels.py
@@ -21,7 +21,7 @@ asignaciones_npo_requiere_visita = AsignacionNpo.objects.filter(estado_asignacio
 asignaciones_npo_en_monitoreo = AsignacionNpo.objects.filter(estado_asignacion=EN_MONITOREO)
 asignaciones_npo_escalado_a_claro = AsignacionNpo.objects.filter(estado_asignacion=ESCALADO_A_CLARO)
 asignaciones_npo_enviado_a_seguimiento = AsignacionNpo.objects.filter(estado_asignacion=ENVIADO_A_SEGUIMIENTO)
-asignaciones_npo_asignada_un_dia = AsignacionNpo.objects.filter(estado_asignacion=ASIGNADA, fecha_asignacion__lte=YESTERDAY)
+asignaciones_npo_asignada_un_dia = AsignacionNpo.objects.filter(estado_asignacion=ASIGNADA, fecha_asignacion__lt=TODAY)
 asignaciones_npo_en_monitoreo_tres_dias = AsignacionNpo.objects.filter(estado_asignacion=EN_MONITOREO, actualizado__lte=THREEDAYS, conceptos_npo__creado__lte=THREEDAYS).distinct()
 
 # asignaciones ni
@@ -31,5 +31,5 @@ asignaciones_ni_requiere_visita = AsignacionNi.objects.filter(estado_asignacion=
 asignaciones_ni_en_monitoreo = AsignacionNi.objects.filter(estado_asignacion=EN_MONITOREO)
 asignaciones_ni_escalado_a_claro = AsignacionNi.objects.filter(estado_asignacion=ESCALADO_A_CLARO)
 asignaciones_ni_enviado_a_seguimiento = AsignacionNi.objects.filter(estado_asignacion=ENVIADO_A_SEGUIMIENTO)
-asignaciones_ni_asignada_un_dia  = AsignacionNi.objects.filter(estado_asignacion=ASIGNADA, fecha_asignacion__lte=YESTERDAY)
+asignaciones_ni_asignada_un_dia  = AsignacionNi.objects.filter(estado_asignacion=ASIGNADA, fecha_asignacion__lt=TODAY)
 asignaciones_ni_en_monitoreo_tres_dias= AsignacionNi.objects.filter(estado_asignacion=EN_MONITOREO, actualizado__lte=THREEDAYS, conceptos_ni__creado__lte=THREEDAYS).distinct()
