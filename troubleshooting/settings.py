b5f3dcb1f17293a2e39069da498500dbd432b061
Julio Brito <jucebridu@gmail.com>
2018-03-10 18:05:25 -0500

    fix error

--- troubleshooting/settings.py
+++ troubleshooting/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ADMINS = os.getenv('ADMINS')
+ADMINS = [('Julio Brito', 'jucebridu@gmail.com')]
 
 ALLOWED_HOSTS = ['*']
 
